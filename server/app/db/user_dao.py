20dfc06dd7a79d4bf3df2d53a9f69783e30502b9
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2024-03-22 17:55:30 +0900

    バグ修正

--- server/app/db/user_dao.py
+++ server/app/db/user_dao.py
@@ -9,8 +9,8 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
     user_id = Column('user_id', Integer, primary_key=True, autoincrement=True)
-    user_name = Column('user_name', String(255), unique=True)
-    password = Column('password', String(255), )
+    user_name = Column('user_name', String(255))
+    password = Column('password', String(255))
     
 class UserDao:
     
@@ -19,22 +19,41 @@ class UserDao:
     
     def create(self, user_name: str, password: str) -> bool:
         try:
+            #ユーザー確認処理
             user = User(user_name=user_name, password=password)
-            self.session.add(user)
-            self.session.commit()
-            return True
+            is_user_exists = self.is_user_exists(user)
+            
+            if is_user_exists is True:
+                self.session.add(user)
+                self.session.commit()
+                print("ユーザー登録完了")
+                return True
+            else:
+                print('存在するユーザーです。')
+                return False
         except SQLAlchemyError as e:
             print(f'ユーザー作成時、エラーが発生しました。:{e}')
+            self.session.rollback()
             return False
+        finally:
+            self.session.close()
     
     def delete(self, user_name:str) -> None:
         try:
             user = User(user_name=user_name)
-            delete_user = self.session.query(User).filter_by(name=user.user_name).first()
+            delete_user = self.session.query(User).filter_by(user_name=user.user_name).first()
             self.session.delete(delete_user)
             self.session.commit()
             return True 
         except SQLAlchemyError as e:
             print(f'ユーザー削除時、エラーが発生しました。:{e}')
             return False
+        finally:
+            self.session.close()
             
+    def is_user_exists(self, user:User) -> bool:
+        query_result = self.session.query(User).filter_by(user_name=user.user_name).first()
+        if query_result is None:
+            return True
+        else:
+            return False
